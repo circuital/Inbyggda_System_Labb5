e5dc577f3e77275a4d1be588235e4241db799da4
circuital <elvira.granqvist@gmail.com>
2020-11-13 11:16:17 +0100

    Created get_button_command() and uart_send_command(), implementented interrupts.

--- Controller/main.c
+++ Controller/main.c
@@ -5,54 +5,22 @@
 
 #include "serial.h"
 #include "buttons.h"
+#include "timer.h"
+
+ISR(TIMER0_COMPA_vect)
+{
+    uart_send_command(get_button_command());
+}
 
 int main(void)
 {
     uart_init();
+    timer_init();
     buttons_init();
 
     while (1)
     {
-        if (PINB & (1 << PB0))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = FORWARD; //FORWARD
-        }
-        else if (PINB & (1 << PB1))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = BACK; //BACK
-        }
-        else if (PINB & (1 << PB2))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = LEFT; //LEFT
-        }
-        else if (PINB & (1 << PB3))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = RIGHT; //RIGHT
-        }
-        else if ((PINB & (1 << PB0)) && (PINB & (1 << PB2)))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = FORWARDLEFT; //FORWARDLEFT
-        }
-        else if ((PINB & (1 << PB0)) && (PINB & (1 << PB3)))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = FORWARDRIGHT; //FORWARDRIGHT
-        }
-        else if ((PINB & (1 << PB1)) && (PINB & (1 << PB2)))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = BACKLEFT; //BACKLEFT
-        }
-        else if ((PINB & (1 << PB1)) && (PINB & (1 << PB3)))
-        {
-            while (!(UCSR0A & (1 << UDRE0)));
-            UDR0 = BACKRIGHT; //BACKRIGHT
-        }
     }
+
     return 0;
 }
