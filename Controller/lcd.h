fc6a30ced22c0167408242f4af3f0ff6b9aaab17
limachar <linn.mann@gmail.com>
2020-11-16 16:38:01 +0100

    LCD implmentation for controller for STEM-mode overview.

---
+++ Controller/lcd.h
@@ -0,0 +1,16 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+void LCD_Init(void);
+
+void LCD_Command(unsigned char cmnd);
+
+void LCD_Char(unsigned char data);
+
+void LCD_Clear();
+
+void LCD_String(char *str);
+
+void LCD_String_xy(char row, char pos, char *str);
+
+#endif //LCD
