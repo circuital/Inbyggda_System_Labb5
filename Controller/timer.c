80e97893ac0ea1895ec9d59fe97934a844767c51
circuital <elvira.granqvist@gmail.com>
2020-11-17 09:25:13 +0100

    Deleted master.c and master.h

--- Controller/timer.c
+++ Controller/timer.c
@@ -14,7 +14,16 @@ void timer_init(void)
 
 	TCNT0 = 0x00; //Timer/Counter Register (stores the counter value). Sätter till 0 för att initiera. 
 	OCR0A = 0x9C; //Output Compare Register. Sätter till 156. 
+	TIMSK0 |= (1 << OCIE0A);//comp_A interrupt
 
     sei();
+
+	////timer 0
+	//TCCR0A |= (1 << WGM01);//CTC mode.
+	//TCCR0B |= ((1 << CS02) | (1 << CS00));//1024
+	//OCR0A = 0x9C;//156, 10ms interrupt
+	//TIMSK0 |= (1 << OCIE0A);//comp_A interrupt
+
+	//sei();
 }
 
